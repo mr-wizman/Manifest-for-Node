1f3a8631ae999e21ab1a04ec707de02e62ba4834
Igor Matyushkin <igormatyushkin014@gmail.com>
2019-07-05 13:54:37 +0300

    Implemented view engines

---
+++ src/view-engines/handlebars-configuration.ts
@@ -0,0 +1,3 @@
+export type HandlebarsConfiguration = {
+	partialsDir: string
+};
---
+++ src/view-engines/index.ts
@@ -0,0 +1,7 @@
+export {
+	ViewEngine
+} from "./view-engine";
+
+export {
+	HandlebarsConfiguration
+} from "./handlebars-configuration";
