705868407cb80fd4e34a71f58d6add381fe1d37a
Igor Matyushkin <igormatyushkin014@gmail.com>
2019-07-05 13:56:21 +0300

    Implemented Manifest type

---
+++ src/configuration/manifest.ts
@@ -0,0 +1,25 @@
+import * as server from "../server";
+
+import * as request from "../request";
+
+import * as routes from "../routes";
+
+import * as viewEngines from "../view-engines";
+
+import * as yandexMetrika from "../vendors/yandex/metrika";
+
+export type Manifest = {
+	readonly server: {
+		readonly port: number,
+		readonly staticLocations: server.StaticLocation[],
+		readonly requestHandlers: request.RequestHandler[],
+		readonly routes: routes.Route[],
+		readonly viewEngines: {
+			readonly handlebars?: viewEngines.HandlebarsConfiguration
+		},
+		readonly currentViewEngine: viewEngines.ViewEngine
+	},
+	readonly analytics: {
+		readonly yandexMetrika?: yandexMetrika.Configuration
+	}
+};
